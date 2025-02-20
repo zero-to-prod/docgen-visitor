90233b0bf47c8b908d149d60a6ac0ab51a7485cc
david_smith <david_smith@sweetwater.com>
2025-02-20 02:29:23 -0500

    feat/add-support-for-functions-and-consts Updated `src/Change.php`.

--- src/Change.php
+++ src/Change.php
@@ -11,38 +11,31 @@ namespace Zerotoprod\DocgenVisitor;
 class Change
 {
     /**
-     * @internal
      * @link https://github.com/zero-to-prod/docgen-visitor
      */
     public const start = 'start';
     /**
-     * @internal
      * @link https://github.com/zero-to-prod/docgen-visitor
      */
     public int $start;
     /**
-     * @internal
      * @link https://github.com/zero-to-prod/docgen-visitor
      */
     public const end = 'end';
     /**
-     * @internal
      * @link https://github.com/zero-to-prod/docgen-visitor
      */
     public int $end;
     /**
-     * @internal
      * @link https://github.com/zero-to-prod/docgen-visitor
      */
     public const text = 'text';
     /**
-     * @internal
      * @link https://github.com/zero-to-prod/docgen-visitor
      */
     public string $text;
 
     /**
-     * @internal
      * @link https://github.com/zero-to-prod/docgen-visitor
      */
     public static function from(array $data): self
